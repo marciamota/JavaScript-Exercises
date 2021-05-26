189595c7bd111ffa7061bc571d2299c441b45c46
Marcia <marcia.garciam@hotmail.com>
2021-05-25 21:40:57 -0500

    second exercise

---
+++ Exercise002/README.md
@@ -0,0 +1,29 @@
+# Exercise 2.1
+# Exercise description
+Find if a given word is inside a given string
+
+## Example 1:
+Input: 
+    character = 'o';
+    string = 'house of blues'
+Output: true
+
+# Exercise 2.2
+# Exercise description
+Find in which position is a given word inside a given string
+
+## Example 1:
+Input: 
+    character = 'o';
+    string = 'house of blues'
+Output: 1
+
+# Exercise 2.3
+# Exercise description
+Find how many times a given word is inside a given string
+
+## Example 1:
+Input: 
+    character = 'o';
+    string = 'house of blues'
+Output: 2
---
+++ Exercise002/Solution01.js
@@ -0,0 +1,26 @@
+// # Exercise description
+// Find if a given character is inside a given string
+
+// ## Example 1:
+// Input: 
+//     character = 'o';
+//     string = 'house of blues'
+// Output: true
+var strToFind = "world";
+var str = "Hello world, welcome to the universe. Another world";
+
+// Solution 2.1
+var n = str.includes(strToFind, 0);
+console.log(n);
+
+// Solution 2.2
+n = str.indexOf(strToFind);
+console.log(n);
+
+// Solution 2.3
+// console.log(str);
+// console.log(str.split(strToFind));
+// console.log(str.split(strToFind).length);
+// console.log(str.split(strToFind).length -1);
+n = str.split(strToFind).length - 1;
+console.log(n);
