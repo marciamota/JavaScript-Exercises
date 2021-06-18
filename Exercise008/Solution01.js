52b604f6845096659848eb0df5a83f1ca95b9c87
Marcia <marcia.garciam@hotmail.com>
2021-06-17 19:10:53 -0500

    exercise 8.2

--- Exercise008/Solution01.js
+++ Exercise008/Solution01.js
@@ -71,20 +71,20 @@
 // Input: arr = ['dog', 'cat', 'dog', 'cat', 'sheep', 'snake']
 // Output: there are 2 dog, 2 cat, sheep 1, snake 1
 
-const animals = ['dog', 'cat', 'dog', 'cat', 'sheep', 'snake'];
+// const animals = ['dog', 'cat', 'dog', 'cat', 'sheep', 'snake'];
 
-let animalsCount = {};
-let species = [];
-let currentAnimal;
-for (let i = 0; i < animals.length; i++) {
-    currentAnimal = animals[i];
-    species = Object.keys(animalsCount);
-    if (!species.includes(currentAnimal)) {
-        animalsCount[currentAnimal] = 1;
-    } else {
-        animalsCount[currentAnimal]++;
-    }
-}
+// let animalsCount = {};
+// let species = [];
+// let currentAnimal;
+// for (let i = 0; i < animals.length; i++) {
+//     currentAnimal = animals[i];
+//     species = Object.keys(animalsCount);
+//     if (!species.includes(currentAnimal)) {
+//         animalsCount[currentAnimal] = 1;
+//     } else {
+//         animalsCount[currentAnimal]++;
+//     }
+// }
 // species = Object.keys(animalsCount);
 // console.log(animalsCount);
 // let message = 'I have ';
@@ -94,6 +94,71 @@ for (let i = 0; i < animals.length; i++) {
 //     message += animalsCount[currentSpecies] + " " + currentSpecies + ", ";
 // }
 // console.log(message);
-for (const animal in animalsCount) {
-    console.log('I have ' + animalsCount[animal] + " " + animal);
+// for (const animal in animalsCount) {
+//     console.log('I have ' + animalsCount[animal] + " " + animal);
+// }
+
+// # Exercise 8.2 part 2
+// having a list of animal quantities in the format below, return the type of animal
+// with more individuals
+// Example
+// input: let animalsCount = {
+//     dog: 2,
+//     cat: 3,
+//     bird: 1
+// }
+// output: 3 cat
+
+// solution 8.2 part 2.1
+// let animalsCount = {
+//     dog: 2,
+//     cat: 3,
+//     fish: 3,
+//     bird: 1
+// }
+// let animalWithMostIndividuals = ''; //  dog, cat
+// let speciesQuantity = 0; // 2, 5, 10
+// for (const animal in animalsCount) {
+//     if (speciesQuantity < animalsCount[animal]) {
+//         animalWithMostIndividuals = animal;
+//         speciesQuantity = animalsCount[animal];
+//     }
+// }
+// console.log('i have ' + speciesQuantity + ' ' + animalWithMostIndividuals);
+
+// solution 8.2 part 2.2
+// let animalsCount = {
+//     dog: 2,
+//     cat: 3,
+//     fish: 3,
+//     bird: 1
+// }
+// let animalWithMostIndividuals = ''; //  dog, cat
+// let speciesQuantity = 0; // 2, 5, 10
+// const species = Object.keys(animalsCount);
+// let currentAnimal;
+// for (let i = 0; i < species.length; i++) {
+//     currentAnimal = species[i];
+//     if (speciesQuantity < animalsCount[currentAnimal]) {
+//         animalWithMostIndividuals = currentAnimal;
+//         speciesQuantity = animalsCount[currentAnimal];
+//     }
+// }
+// console.log('i have ' + speciesQuantity + ' ' + animalWithMostIndividuals);
+
+// solution 8.2 part 2.3
+let animalsCount = {
+    dog: 2,
+    cat: 3,
+    fish: 3,
+    bird: 1
 }
+let animalWithMostIndividuals = ''; //  dog, cat
+let speciesQuantity = 0; // 2, 5, 10
+for (const [animal, animalQuantity] of Object.entries(animalsCount)) {
+    if (speciesQuantity < animalQuantity) {
+        animalWithMostIndividuals = animal;
+        speciesQuantity = animalQuantity;
+    }
+}
+console.log('i have ' + speciesQuantity + ' ' + animalWithMostIndividuals);
