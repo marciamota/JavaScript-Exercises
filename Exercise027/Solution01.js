f9f11a35f4ce9e4816512b9393c6b5bb8a79220a
Marcia <marcia.garciam@hotmail.com>
2021-08-26 21:01:19 -0500

    exercise 27

---
+++ Exercise027/Solution01.js
@@ -0,0 +1,46 @@
+// Create a function that calculates the area of a rectangle. If the arguments are invalid, your function must return -1.
+
+// Examples
+// area(3, 4) ➞ 12
+
+// area(10, 11) ➞ 110
+
+// area(-1, 5) ➞ -1
+
+// area(-3, 5) ➞ -1
+
+// area(-3, -5) ➞ -1
+
+// area(3, -5) ➞ -1
+
+// area(0, 2) ➞ -1
+
+// function area (base, height){
+//     if (base > 0 && height > 0) {
+//         return base * height;
+//     }
+//     // if (base <= 0 || height <= 0){
+//     //     return base * height;
+//     // } 
+//     return -1;
+// }
+// console.log(area(1,0));
+
+// ##########################################################################################
+// Given two strings, firstName and lastName, return a single string in the format "last, first".
+
+// Examples
+// concatName("First", "Last") ➞ "Last, First"
+
+// concatName("John", "Doe") ➞ "Doe, John"
+
+// concatName("Mary", "Jane") ➞ "Jane, Mary"
+
+// function concatName(lastname, firstname){
+//     return lastname + ', ' + firstname;
+// }
+// console.log(concatName("Gael","Mary"));
+
+// ##################################################################################################
+
+Concatenate First and Last Name into One String
