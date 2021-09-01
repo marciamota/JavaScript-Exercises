506106a43d2c2eae7e7d9b83770d2ead5d7ae08a
Marcia <marcia.garciam@hotmail.com>
2021-08-31 19:35:44 -0500

    exercise 28

---
+++ Exercise028/Solution01.js
@@ -0,0 +1,141 @@
+// How Edabit Works
+// This is an introduction to how challenges on Edabit work. 
+// In the Code tab above you'll see a starter function that looks like this:
+
+// function hello() {
+
+// }
+// All you have to do is type return "hello edabit.com" between the curly braces { }
+
+// function hello(){
+//     return "hello edabit.com";
+// }
+// console.log(hello());
+
+// ########################################################################################
+// Return the Sum of Two Numbers
+// Create a function that takes two numbers as arguments and return their sum.
+
+// Examples
+// addition(3, 2) ➞ 5
+
+// addition(-3, -6) ➞ -9
+
+// addition(7, 3) ➞ 10
+
+// function addition(num1, num2){
+//     return num1 + num2;
+// }
+// console.log(addition(3,3));
+
+
+// ##########################################################################################
+// Convert Minutes into Seconds
+// Write a function that takes an integer minutes and converts it to seconds.
+
+// Examples
+// convert(5) ➞ 300
+
+// convert(3) ➞ 180
+
+// convert(2) ➞ 120
+
+// function convert (num1){
+//     return num1 * 60;
+// }
+// console.log(convert(10));
+
+// #####################################################################################
+// Return the Next Number from the Integer Passed
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// Examples
+// addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+// addition(-3) ➞ -2
+
+// function addition (num1){
+//     return num1 +1;
+// }
+// console.log(addition(10));
+
+// #####################################################################################
+// Area of a Triangle
+// Write a function that takes the base and height of a triangle and return its area.
+
+// Examples
+// triArea(3, 2) ➞ 3
+
+// triArea(7, 4) ➞ 14
+
+// triArea(10, 10) ➞ 50
+
+// function triArea (base, height){
+//     return (base * height) /2;
+// }
+// console.log(triArea(10,2));
+
+// #########################################################################################
+// Convert Age to Days
+// Create a function that takes the age in years and returns the age in days.
+
+// Examples
+// calcAge(65) ➞ 23725
+
+// calcAge(0) ➞ 0
+
+// calcAge(20) ➞ 7300
+
+// function calcAge(age){
+//     return age * 365;
+// }
+// console.log(calcAge(20));
+
+// ###########################################################################################
+// Return the First Element in an Array
+// Create a function that takes an array containing only numbers and return the first element.
+
+// Examples
+// getFirstValue([1, 2, 3]) ➞ 1
+
+// getFirstValue([80, 5, 100]) ➞ 80
+
+// getFirstValue([-500, 0, 50]) ➞ -500
+
+// function getFirstValue(myArray){
+//     return myArray[0];
+// }
+// console.log(getFirstValue([25,10,5]));
+
+// ############################################################################################
+// Power Calculator
+// Create a function that takes voltage and current and returns the calculated power.
+
+// Examples
+// circuitPower(230, 10) ➞ 2300
+
+// circuitPower(110, 3) ➞ 330
+
+// circuitPower(480, 20) ➞ 9600
+
+// function circuitPower(voltage, current){
+//     return voltage * current;
+// }
+// console.log(circuitPower(220,10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
