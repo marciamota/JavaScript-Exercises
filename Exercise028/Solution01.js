ce783e2ea42b6851564595077c706179a39b44eb
Marcia <marcia.garciam@hotmail.com>
2021-09-27 20:11:22 -0500

    exercise 28

--- Exercise028/Solution01.js
+++ Exercise028/Solution01.js
@@ -986,6 +986,33 @@
 // }
 // console.log(shouldServeDrinks(19, false));
 
+// ############################################################################################
+
+// Format I: Template String
+// Write a template string according to the following example:
+
+// Example
+// const a = "John";
+// const b = "Joe";
+// const c = "Jack";
+// const template = "your template string" ➞ "Their names were:  John,  Joe  and  Jack."
+
+// const a = "John";
+// const b = "Joe";
+// const c = "Jack";
+// const template = `Their names were ${a}, ${b} and ${c}.`;
+
+// console.log(template);
+
+// ############################################################################################
+
+// Arrow Functions
+
+
+
+
+
+
 
 
 
